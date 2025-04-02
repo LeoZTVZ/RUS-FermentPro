f1b96529ab7a34f937743af3681c82d259c150c1
AntonioMiser0 <amiser@tvz.hr>
2025-04-03 00:22:55 +0200

    Gotov Lab1

--- Antonio_Miser_Lab1/sketch.ino
+++ Antonio_Miser_Lab1/sketch.ino
@@ -1,61 +1,128 @@
-// Arduino Uno - Višestruki interrupti s LED-icama i buzzerom (Wokwi)
+#include <Arduino.h>
 
-const int led1 = 9;  // LED dioda za tipkalo
-const int led2 = 10; // LED dioda za timer
-const int buzzer = 11; // Buzzer povezan na digitalni pin 11
-const int buttonPin = 2; // Tipkalo povezano na digitalni pin 2
-volatile bool buttonPressed = false; // Varijabla za praćenje statusa tipkala
+// Definicija pinova za LED diode, buzzer i tipkala
+const int led1 = 9;
+const int led2 = 10;
+const int led3 = 8;
+const int buzzer = 11;
+const int button1Pin = 2;
+const int button2Pin = 3;
+const int debounceTime = 200; // Vrijeme debounce-a za tipkala
+
+// Varijable za praćenje statusa tipkala i buzzera
+volatile bool button1Pressed = false;
+volatile bool button2Pressed = false;
 volatile int buzzerFrequency = 500; // Početna frekvencija buzzera
+volatile bool buzzerLimitReached = false; // Oznaka je li dostignuta maksimalna frekvencija
 
 void setup() {
-    pinMode(led1, OUTPUT); // Postavljanje pina za LED1 kao izlaz
-    pinMode(led2, OUTPUT); // Postavljanje pina za LED2 kao izlaz
+    // Postavljanje pinova za LED diode kao izlaze
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(led3, OUTPUT);
     pinMode(buzzer, OUTPUT); // Postavljanje pina za buzzer kao izlaz
-    pinMode(buttonPin, INPUT_PULLUP); // Omogućavanje pull-up otpornika za tipkalo
+    pinMode(button1Pin, INPUT_PULLUP); // Postavljanje tipkala 1 kao ulaz s pull-up otpornikom
+    pinMode(button2Pin, INPUT_PULLUP); // Postavljanje tipkala 2 kao ulaz s pull-up otpornikom
     
-    attachInterrupt(digitalPinToInterrupt(buttonPin), buttonISR, FALLING); // Eksterni prekid na tipkalu
+    // Postavljanje prekida za tipkala
+    attachInterrupt(digitalPinToInterrupt(button1Pin), button1ISR, FALLING);
+    attachInterrupt(digitalPinToInterrupt(button2Pin), button2ISR, FALLING);
     
-    Timer1Setup(); // Postavljanje timera
-    Serial.begin(9600); // Pokretanje serijske komunikacije
+    // Inicijalizacija Timer1 za periodične prekide
+    Timer1Setup();
+    Serial.begin(9600); // Pokretanje serijske komunikacije za ispis podataka
 }
 
 void loop() {
-    if (buttonPressed) {
-        digitalWrite(led1, HIGH); // Uključuje LED1
-        digitalWrite(led2, HIGH); // Uključuje LED2
-        tone(buzzer, buzzerFrequency); // Postavlja buzzer na trenutnu frekvenciju
-    } else {
-        digitalWrite(led1, LOW); // Isključuje LED1
-        noTone(buzzer); // Isključuje buzzer
+
+        // Upravljanje LED1 i buzzerom ako je tipkalo 1 pritisnuto
+        if (button1Pressed) {
+            digitalWrite(led1, HIGH);
+            digitalWrite(led2, HIGH);
+            tone(buzzer, buzzerFrequency); // Postavljanje frekvencije buzzera
+        } else {
+            digitalWrite(led1, LOW);
+            noTone(buzzer);
+        }
+
+        // Upravljanje LED3 ako je tipkalo 2 pritisnuto, ali tipkalo 1 nije
+        if (button2Pressed && !button1Pressed) {
+            digitalWrite(led3, HIGH);
+        } else {
+            digitalWrite(led3, LOW);
+        }
+    
+}
+
+// ISR prekidna rutina za tipkalo s višim prioritetom
+void button1ISR() {
+    static unsigned long lastInterruptTime = 0;
+    unsigned long interruptTime = millis();
+    if (interruptTime - lastInterruptTime > debounceTime) {
+        button1Pressed = !button1Pressed;
+        Serial.print("Button visi prioritet: ");
+        Serial.println(button1Pressed ? "true" : "false");
     }
+    lastInterruptTime = interruptTime;
 }
 
-// ISR prekidna rutina za tipkalo
-void buttonISR() {
-    buttonPressed = !buttonPressed; // Mijenja stanje varijable buttonPressed
-    Serial.print("Button pressed: ");
-    Serial.println(buttonPressed ? "true" : "false");
+// ISR prekidna rutina za tipkalo s nižim prioritetom
+void button2ISR() {
+    static unsigned long lastInterruptTime = 0;
+    unsigned long interruptTime = millis();
+    if (interruptTime - lastInterruptTime > debounceTime) {
+        button2Pressed = !button2Pressed;
+        Serial.print("Button nizi prioritet: ");
+        Serial.println(button2Pressed ? "true" : "false");
+    }
+    lastInterruptTime = interruptTime;
 }
 
-// Funkcija za postavljanje Timer1 koji generira prekid svake 500 ms
+// Postavljanje Timer1 koji generira prekid svake 500 ms
 void Timer1Setup() {
     noInterrupts(); // Onemogućavanje prekida tijekom postavljanja timera
-    TCCR1A = 0; // Normalni način rada
-    TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10); // CTC način rada, prescaler 1024
-    OCR1A = 7812; // Vrijednost za 500 ms (16 MHz / 1024 prescaler / 2 Hz)
-    TIMSK1 |= (1 << OCIE1A); // Omogućavanje Timer1 compare interrupta
+    TCCR1A = 0; //Postavlja registar kontrolera timera 1 na nulu
+    TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10); 
+       /*
+    Postavlja bit za CTC (Clear Timer on Compare Match) način rada, 
+    koji omogućuje timeru da generira prekid kada se 
+    brojčanik (counter) podudari s određenom vrijednošću.
+    
+    Postavlja prescaler timeru na 1024, što znači da će timer brojači biti 
+    povećani za svakih 1024 takta procesora.  
+    Time se usporava brojanje timera, što daje točno vrijeme za generiranje prekida.
+    */
+    OCR1A = 7812; // Vrijednost za 500 ms 
+    TIMSK1 |= (1 << OCIE1A); // 
+    /*Omogućuje prekid na usporedbi (compare interrupt) za Timer1. 
+    Kada brojčanik dosegne vrijednost u OCR1A, 
+    generirat će se prekid i pozvat će se odgovarajući prekidni rukovatelj.
+    */
     interrupts(); // Ponovno uključivanje prekida
 }
 
 // ISR prekidna rutina za Timer1
 ISR(TIMER1_COMPA_vect) {
-    if (!buttonPressed) { // Ako tipka NIJE pritisnuta, LED2 mijenja stanje svake sekunde
-        digitalWrite(led2, !digitalRead(led2));
-    } else { 
-        static int step = 10;
-        buzzerFrequency += step; // Mijenja frekvenciju svake sekunde
+    static int step = 100;
+    if (!button1Pressed) {
+        toggleLED(led2); // Treptanje LED2 ako tipkalo 1 nije pritisnuto
+    } else {
+        buzzerFrequency += step; // Mijenjanje frekvencije buzzera
         if (buzzerFrequency >= 1500 || buzzerFrequency <= 500) {
             step = -step;
         }
+        if (buzzerFrequency >= 1500) {
+            buzzerLimitReached = true; // Postavljanje oznake da je buzzer dostigao 1500 Hz
+            Serial.print("Buzzer: ");
+            Serial.println(buzzerLimitReached ? "true" : "false");
+        } else {
+            buzzerLimitReached = false;
+        }
     }
 }
+
+// Funkcija za promjenu stanja određene LED diode
+void toggleLED(int ledPin) {
+    digitalWrite(ledPin, !digitalRead(ledPin));
+}
+
