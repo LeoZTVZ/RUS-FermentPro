cc1d59c04d476c0db3fb7cc5a31c21e0ba962929
amiser11 <amiser11@users.noreply.github.com>
2025-04-10 15:17:21 +0000

    deploy: 2ee3a183ad6001cde1813ced181e4dc49b025dae

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,9 +1,10 @@
 var indexSectionsWithContent =
 {
-  0: "abegilpst",
-  1: "s",
-  2: "beils",
-  3: "abgilpt"
+  0: "bilpstz",
+  1: "z",
+  2: "ilps",
+  3: "blpst",
+  4: "i"
 };
 
 var indexSectionNames =
@@ -11,7 +12,8 @@ var indexSectionNames =
   0: "all",
   1: "files",
   2: "functions",
-  3: "variables"
+  3: "variables",
+  4: "defines"
 };
 
 var indexSectionLabels =
@@ -19,6 +21,7 @@ var indexSectionLabels =
   0: "All",
   1: "Files",
   2: "Functions",
-  3: "Variables"
+  3: "Variables",
+  4: "Macros"
 };
 
