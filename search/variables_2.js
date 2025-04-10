b5c2e173811c8e9104543983d1293f6d77c64e1a
amiser11 <amiser11@users.noreply.github.com>
2025-04-10 14:55:44 +0000

    deploy: ecb0f02071233531aa4c268d380189f44e09f88d

--- search/variables_2.js
+++ search/variables_2.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['led1_0',['led1',['../da/d98/sketch_8ino.html#aa0648cd6a66d082cd9c9de944c92df3a',1,'sketch.ino']]],
-  ['led2_1',['led2',['../da/d98/sketch_8ino.html#a0e6f4a01be910bece324d06d3d4851e8',1,'sketch.ino']]],
-  ['led3_2',['led3',['../da/d98/sketch_8ino.html#a9212a56246c1f98c2c0186b87e50de38',1,'sketch.ino']]]
+  ['previousmillis_0',['previousMillis',['../d6/d3b/zad_8ino.html#a86027ad9c9159f02624d136fb63364fe',1,'zad.ino']]]
 ];
