0262d7ab274d037c9753f7a1f1c93c83b6a43cf2
amiser11 <amiser11@users.noreply.github.com>
2025-04-10 14:15:53 +0000

    deploy: a967fdad9293eb5f9ad24a3fd72b112fc556e31a

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['interval_0',['INTERVAL',['../d6/d3b/zad_8ino.html#ab39fec97d85960796efedec442f38004',1,'zad.ino']]],
-  ['isr_1',['isr',['../d6/d3b/zad_8ino.html#ad39420cdd896dd12c68e36313139d0a5',1,'ISR(TIMER1_COMPA_vect):&#160;zad.ino'],['../d6/d3b/zad_8ino.html#a05c2e5b588ced1cd7312f5b0edc5b295',1,'ISR(ADC_vect):&#160;zad.ino']]],
-  ['isr_5fbutton_2',['ISR_Button',['../d6/d3b/zad_8ino.html#a065c3383f054ebc9f42862da6aa94483',1,'zad.ino']]]
+  ['blinkcount_0',['blinkCount',['../da/d98/sketch_8ino.html#a730cd5ddcd07f770e058e1c8aecdb41a',1,'sketch.ino']]],
+  ['buttonpin_1',['buttonPin',['../da/d98/sketch_8ino.html#a4ddb8b6ae564eb22f7c74f2683a63b8e',1,'sketch.ino']]],
+  ['buttonwakeup_2',['buttonWakeUp',['../da/d98/sketch_8ino.html#a6becb685a80d2b04d13e90291e3a059f',1,'sketch.ino']]],
+  ['buttonwokeup_3',['buttonWokeUp',['../da/d98/sketch_8ino.html#a61b65db2045e38085041a76da7437db0',1,'sketch.ino']]]
 ];
