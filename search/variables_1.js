9a8938248cd0483e8b7c59aed4fe1aa46435a6dc
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-10 15:06:10 +0000

    deploy: 269c8626771e792b9af5e8ffeb4c4ddf46189635

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['blinkcount_0',['blinkCount',['../da/d98/sketch_8ino.html#a730cd5ddcd07f770e058e1c8aecdb41a',1,'sketch.ino']]],
-  ['buttonpin_1',['buttonPin',['../da/d98/sketch_8ino.html#a4ddb8b6ae564eb22f7c74f2683a63b8e',1,'sketch.ino']]],
-  ['buttonwokeup_2',['buttonWokeUp',['../da/d98/sketch_8ino.html#a61b65db2045e38085041a76da7437db0',1,'sketch.ino']]]
+  ['ledstate_0',['ledState',['../d6/d3b/zad_8ino.html#a721652ea8495ead65724ceff69063b9a',1,'zad.ino']]],
+  ['lightdetected_1',['lightDetected',['../d6/d3b/zad_8ino.html#a68cc068e4925dc3bf3f70a7bd9e11420',1,'zad.ino']]]
 ];
