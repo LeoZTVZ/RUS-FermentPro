d262b51fb4c8e9fb4719bbfd9e07b2167786fd58
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-10 14:59:19 +0000

    deploy: 4321cf01248829fb3d39e451ca66c65ba96f2103

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['debouncetime_0',['debounceTime',['../da/d98/sketch_8ino.html#a7a3a605906f6ba4e357a32fe92476387',1,'sketch.ino']]]
+  ['ledstate_0',['ledState',['../d6/d3b/zad_8ino.html#a721652ea8495ead65724ceff69063b9a',1,'zad.ino']]],
+  ['lightdetected_1',['lightDetected',['../d6/d3b/zad_8ino.html#a68cc068e4925dc3bf3f70a7bd9e11420',1,'zad.ino']]]
 ];
