29fd0d087ea0264e51bfb9d70e174573cd00f379
amiser11 <amiser11@users.noreply.github.com>
2025-04-10 14:13:50 +0000

    deploy: 9fbf4feeff4544c3fde9ab526c0e21e98ac3eb73

--- search/all_6.js
+++ search/all_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['previousmillis_0',['previousMillis',['../da/d98/sketch_8ino.html#a86027ad9c9159f02624d136fb63364fe',1,'sketch.ino']]]
+  ['zad_2eino_0',['zad.ino',['../d6/d3b/zad_8ino.html',1,'']]]
 ];
