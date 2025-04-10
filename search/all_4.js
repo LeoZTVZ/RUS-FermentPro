d3bb45d57003486538e5a79e2cecd587789cfc85
amiser11 <amiser11@users.noreply.github.com>
2025-04-10 14:56:36 +0000

    deploy: 9f7431ff00a0c32b0820b49730837fb3b99ed65a

--- search/all_4.js
+++ search/all_4.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['inactiveperiod_0',['inActivePeriod',['../da/d98/sketch_8ino.html#a143098743536b1b217aeef84a8a19335',1,'sketch.ino']]],
-  ['interval_1',['interval',['../da/d98/sketch_8ino.html#afab23e0ee85e56261dace1dba900e39e',1,'sketch.ino']]],
-  ['isr_2',['ISR',['../da/d98/sketch_8ino.html#a43905d6fb5c4d433a49f527fa6ab811f',1,'sketch.ino']]]
+  ['setup_0',['setup',['../da/d98/sketch_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'sketch.ino']]],
+  ['sketch_2eino_1',['sketch.ino',['../da/d98/sketch_8ino.html',1,'']]]
 ];
