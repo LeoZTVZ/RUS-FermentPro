c809422bfe855e63866e2e8ba49f1c1bfb37d3e7
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-10 15:02:17 +0000

    deploy: 63a4eaf19d412adfea2e4b50a6c4a62b57246e50

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['debouncetime_0',['debounceTime',['../da/d98/sketch_8ino.html#a7a3a605906f6ba4e357a32fe92476387',1,'sketch.ino']]]
+  ['blinkcount_0',['blinkCount',['../da/d98/sketch_8ino.html#a730cd5ddcd07f770e058e1c8aecdb41a',1,'sketch.ino']]],
+  ['buttonpin_1',['buttonPin',['../da/d98/sketch_8ino.html#a4ddb8b6ae564eb22f7c74f2683a63b8e',1,'sketch.ino']]],
+  ['buttonwokeup_2',['buttonWokeUp',['../da/d98/sketch_8ino.html#a61b65db2045e38085041a76da7437db0',1,'sketch.ino']]]
 ];
