04cc1deb131c175703eef494096487ef4a47fbd2
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-10 15:02:09 +0000

    deploy: 63a4eaf19d412adfea2e4b50a6c4a62b57246e50

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['buttonwakeup_0',['buttonWakeUp',['../da/d98/sketch_8ino.html#a6becb685a80d2b04d13e90291e3a059f',1,'sketch.ino']]]
+  ['isr_0',['isr',['../d6/d3b/zad_8ino.html#ad39420cdd896dd12c68e36313139d0a5',1,'ISR(TIMER1_COMPA_vect):&#160;zad.ino'],['../d6/d3b/zad_8ino.html#a05c2e5b588ced1cd7312f5b0edc5b295',1,'ISR(ADC_vect):&#160;zad.ino']]],
+  ['isr_5fbutton_1',['ISR_Button',['../d6/d3b/zad_8ino.html#a065c3383f054ebc9f42862da6aa94483',1,'zad.ino']]]
 ];
