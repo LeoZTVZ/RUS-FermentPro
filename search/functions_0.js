98f1392432da64a1584069647ed577778fe71eb2
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-15 16:45:55 +0000

    deploy: a3cd92a1ed1a4184e2e99263f82a888a3da2954f

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['button1isr_0',['button1ISR',['../da/d98/sketch_8ino.html#ae2e7584d3d26e6dd0163d2735c466a17',1,'sketch.ino']]],
-  ['button2isr_1',['button2ISR',['../da/d98/sketch_8ino.html#a1070645e82733de46f47b7432972a237',1,'sketch.ino']]]
+  ['buttonwakeup_0',['buttonWakeUp',['../da/d98/sketch_8ino.html#a6becb685a80d2b04d13e90291e3a059f',1,'sketch.ino']]]
 ];
---
+++ search/variables_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['ledpin_0',['ledPin',['../da/d98/sketch_8ino.html#a2cd9f0d96c9cd0637798de3baa7aee60',1,'sketch.ino']]],
+  ['ledstate_1',['ledState',['../da/d98/sketch_8ino.html#ad43bbd53ee15610b1c213b0b6412d175',1,'sketch.ino']]]
+];
