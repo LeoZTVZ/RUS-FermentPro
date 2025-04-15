6fd76de0ba99bcabc6dbd1b43a5163c45fe510a9
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-15 17:15:25 +0000

    deploy: 1d347adf1704231f4fb75fc6e7d63b40e03f5f72

--- search/all_3.js
+++ search/all_3.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['gosleep_0',['goSleep',['../da/d98/sketch_8ino.html#a9a3c15cd67dbd449d48af644ca9ed8b0',1,'sketch.ino']]]
+  ['led1_0',['led1',['../da/d98/sketch_8ino.html#aa0648cd6a66d082cd9c9de944c92df3a',1,'sketch.ino']]],
+  ['led2_1',['led2',['../da/d98/sketch_8ino.html#a0e6f4a01be910bece324d06d3d4851e8',1,'sketch.ino']]],
+  ['led3_2',['led3',['../da/d98/sketch_8ino.html#a9212a56246c1f98c2c0186b87e50de38',1,'sketch.ino']]],
+  ['loop_3',['loop',['../da/d98/sketch_8ino.html#afe461d27b9c48d5921c00d521181f12f',1,'sketch.ino']]]
 ];
