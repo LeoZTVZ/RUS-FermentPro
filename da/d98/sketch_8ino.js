d17aa631feb005077250256534f04c01bcf5de9a
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-15 17:13:49 +0000

    deploy: b643cb7a5882091cfa91fd2650644c4a5a682827

---
+++ da/d98/sketch_8ino.js
@@ -0,0 +1,20 @@
+var sketch_8ino =
+[
+    [ "buttonWakeUp", "da/d98/sketch_8ino.html#a6becb685a80d2b04d13e90291e3a059f", null ],
+    [ "enterSleep", "da/d98/sketch_8ino.html#ae5b5093bef9a291e112c49e9aae5feec", null ],
+    [ "ISR", "da/d98/sketch_8ino.html#a43905d6fb5c4d433a49f527fa6ab811f", null ],
+    [ "loop", "da/d98/sketch_8ino.html#afe461d27b9c48d5921c00d521181f12f", null ],
+    [ "setup", "da/d98/sketch_8ino.html#a4fc01d736fe50cf5b977f755b675f11d", null ],
+    [ "setupWatchdog", "da/d98/sketch_8ino.html#a476ccd62c199f330f95bce19c498e3e9", null ],
+    [ "activePeriodCount", "da/d98/sketch_8ino.html#a94d8527f0b3fc649d1d144aa0db84944", null ],
+    [ "blinkCount", "da/d98/sketch_8ino.html#a730cd5ddcd07f770e058e1c8aecdb41a", null ],
+    [ "buttonPin", "da/d98/sketch_8ino.html#a4ddb8b6ae564eb22f7c74f2683a63b8e", null ],
+    [ "buttonWokeUp", "da/d98/sketch_8ino.html#a61b65db2045e38085041a76da7437db0", null ],
+    [ "goSleep", "da/d98/sketch_8ino.html#a9a3c15cd67dbd449d48af644ca9ed8b0", null ],
+    [ "inActivePeriod", "da/d98/sketch_8ino.html#a143098743536b1b217aeef84a8a19335", null ],
+    [ "interval", "da/d98/sketch_8ino.html#afab23e0ee85e56261dace1dba900e39e", null ],
+    [ "ledPin", "da/d98/sketch_8ino.html#a2cd9f0d96c9cd0637798de3baa7aee60", null ],
+    [ "ledState", "da/d98/sketch_8ino.html#ad43bbd53ee15610b1c213b0b6412d175", null ],
+    [ "previousMillis", "da/d98/sketch_8ino.html#a86027ad9c9159f02624d136fb63364fe", null ],
+    [ "timerWokeUp", "da/d98/sketch_8ino.html#a7c2785434d166188ede44ab2ec79a8af", null ]
+];
