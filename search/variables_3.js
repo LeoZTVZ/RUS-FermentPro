23effab8b449245582890f6988bb7cf5fa1f25bd
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-16 13:50:55 +0000

    deploy: 6849b36dedffc41a94f3e6f77c4ab6874f843728

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['inactiveperiod_0',['inActivePeriod',['../da/d98/sketch_8ino.html#a143098743536b1b217aeef84a8a19335',1,'sketch.ino']]],
-  ['interval_1',['interval',['../da/d98/sketch_8ino.html#afab23e0ee85e56261dace1dba900e39e',1,'sketch.ino']]]
+  ['sensorready_0',['sensorReady',['../d6/d3b/zad_8ino.html#abde7e55016ffcd8be7f39e5f393ff044',1,'zad.ino']]]
 ];
