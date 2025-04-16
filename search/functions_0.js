629e457b521fb61fe29061cf2e0e215fe87ce2fc
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-16 13:52:04 +0000

    deploy: bfabec6adce54dad2d72abb10f08fdbc4d77faac

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['button1isr_0',['button1ISR',['../da/d98/sketch_8ino.html#ae2e7584d3d26e6dd0163d2735c466a17',1,'sketch.ino']]],
-  ['button2isr_1',['button2ISR',['../da/d98/sketch_8ino.html#a1070645e82733de46f47b7432972a237',1,'sketch.ino']]]
+  ['isr_0',['isr',['../d6/d3b/zad_8ino.html#ad39420cdd896dd12c68e36313139d0a5',1,'ISR(TIMER1_COMPA_vect):&#160;zad.ino'],['../d6/d3b/zad_8ino.html#a05c2e5b588ced1cd7312f5b0edc5b295',1,'ISR(ADC_vect):&#160;zad.ino']]],
+  ['isr_5fbutton_1',['ISR_Button',['../d6/d3b/zad_8ino.html#a065c3383f054ebc9f42862da6aa94483',1,'zad.ino']]]
 ];
