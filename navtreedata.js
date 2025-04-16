76499c470a5bcb31ef2aeb017380aeaf855aec25
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-16 13:51:48 +0000

    deploy: 0bb179103e147622b6ca319c70843365c5ae5ba8

--- navtreedata.js
+++ navtreedata.js
@@ -30,7 +30,8 @@ var NAVTREE =
       [ "Globals", "globals.html", [
         [ "All", "globals.html", null ],
         [ "Functions", "globals_func.html", null ],
-        [ "Variables", "globals_vars.html", null ]
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
       ] ]
     ] ]
   ] ]
@@ -38,7 +39,7 @@ var NAVTREE =
 
 var NAVTREEINDEX =
 [
-"da/d98/sketch_8ino.html"
+"d6/d3b/zad_8ino.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
