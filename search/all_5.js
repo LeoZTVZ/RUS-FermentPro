ae931cd883498b36f9a0888af6774beafffc3027
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-17 14:06:02 +0000

    deploy: 39fd42b9005a04699aacfddc541f670dd66ab605

--- search/all_5.js
+++ search/all_5.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['timertriggered_0',['timerTriggered',['../d6/d3b/zad_8ino.html#a649f2fe86e0d0b3887bc7e9d22cc0895',1,'zad.ino']]]
+  ['ledpin_0',['ledPin',['../da/d98/sketch_8ino.html#a2cd9f0d96c9cd0637798de3baa7aee60',1,'sketch.ino']]],
+  ['ledstate_1',['ledState',['../da/d98/sketch_8ino.html#ad43bbd53ee15610b1c213b0b6412d175',1,'sketch.ino']]],
+  ['loop_2',['loop',['../da/d98/sketch_8ino.html#afe461d27b9c48d5921c00d521181f12f',1,'sketch.ino']]]
 ];
