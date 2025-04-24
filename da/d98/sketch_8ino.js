e6ac442d0a64021badfef15e7f46d92ea75b297d
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-24 11:27:56 +0000

    deploy: 6ec387300efe7f1eb589b65fe25b02754fff8989

---
+++ da/d98/sketch_8ino.js
@@ -0,0 +1,21 @@
+var sketch_8ino =
+[
+    [ "button1ISR", "da/d98/sketch_8ino.html#ae2e7584d3d26e6dd0163d2735c466a17", null ],
+    [ "button2ISR", "da/d98/sketch_8ino.html#a1070645e82733de46f47b7432972a237", null ],
+    [ "ISR", "da/d98/sketch_8ino.html#ad39420cdd896dd12c68e36313139d0a5", null ],
+    [ "loop", "da/d98/sketch_8ino.html#afe461d27b9c48d5921c00d521181f12f", null ],
+    [ "setup", "da/d98/sketch_8ino.html#a4fc01d736fe50cf5b977f755b675f11d", null ],
+    [ "Timer1Setup", "da/d98/sketch_8ino.html#a6da768d9996fe5c028404077274e5b4b", null ],
+    [ "toggleLED", "da/d98/sketch_8ino.html#a4bbc5d148cb44b6581eb6cdf3ac86534", null ],
+    [ "button1Pin", "da/d98/sketch_8ino.html#a4f40099c2dd0645b871e53ce0700370b", null ],
+    [ "button1Pressed", "da/d98/sketch_8ino.html#a4345c2c6b858f221ba8e54ed3b3da4e8", null ],
+    [ "button2Pin", "da/d98/sketch_8ino.html#a73a6345a84cc3b11e164dbcace03472a", null ],
+    [ "button2Pressed", "da/d98/sketch_8ino.html#afd5a2ae158bc6a9c916a4e49e0e65e66", null ],
+    [ "buzzer", "da/d98/sketch_8ino.html#aa19b07c6332e4fb436ceea2ff500427b", null ],
+    [ "buzzerFrequency", "da/d98/sketch_8ino.html#a2c542851286ae33b7a463fd862321d4c", null ],
+    [ "buzzerLimitReached", "da/d98/sketch_8ino.html#a1be3f094c74795688fda01950e368905", null ],
+    [ "debounceTime", "da/d98/sketch_8ino.html#a7a3a605906f6ba4e357a32fe92476387", null ],
+    [ "led1", "da/d98/sketch_8ino.html#aa0648cd6a66d082cd9c9de944c92df3a", null ],
+    [ "led2", "da/d98/sketch_8ino.html#a0e6f4a01be910bece324d06d3d4851e8", null ],
+    [ "led3", "da/d98/sketch_8ino.html#a9212a56246c1f98c2c0186b87e50de38", null ]
+];
