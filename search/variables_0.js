47d040dde2a6608cfba25d59854636ac7badfcd2
amiser11 <amiser11@users.noreply.github.com>
2025-04-26 11:51:46 +0000

    deploy: 140c72c209fc1a0b57059feef3f768cac0438c82

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,10 +1,4 @@
 var searchData=
 [
-  ['button1pin_0',['button1Pin',['../da/d98/sketch_8ino.html#a4f40099c2dd0645b871e53ce0700370b',1,'sketch.ino']]],
-  ['button1pressed_1',['button1Pressed',['../da/d98/sketch_8ino.html#a4345c2c6b858f221ba8e54ed3b3da4e8',1,'sketch.ino']]],
-  ['button2pin_2',['button2Pin',['../da/d98/sketch_8ino.html#a73a6345a84cc3b11e164dbcace03472a',1,'sketch.ino']]],
-  ['button2pressed_3',['button2Pressed',['../da/d98/sketch_8ino.html#afd5a2ae158bc6a9c916a4e49e0e65e66',1,'sketch.ino']]],
-  ['buzzer_4',['buzzer',['../da/d98/sketch_8ino.html#aa19b07c6332e4fb436ceea2ff500427b',1,'sketch.ino']]],
-  ['buzzerfrequency_5',['buzzerFrequency',['../da/d98/sketch_8ino.html#a2c542851286ae33b7a463fd862321d4c',1,'sketch.ino']]],
-  ['buzzerlimitreached_6',['buzzerLimitReached',['../da/d98/sketch_8ino.html#a1be3f094c74795688fda01950e368905',1,'sketch.ino']]]
+  ['activeperiodcount_0',['activePeriodCount',['../da/d98/sketch_8ino.html#a94d8527f0b3fc649d1d144aa0db84944',1,'sketch.ino']]]
 ];
