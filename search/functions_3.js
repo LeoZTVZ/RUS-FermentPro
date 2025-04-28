233b0d6f32198d44967d1d3a94617a539c842453
amiser11 <amiser11@users.noreply.github.com>
2025-04-28 06:04:27 +0000

    deploy: 9b0ba4556bb0ba089facfc465d7bebf6eaed2f5b

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['loop_0',['loop',['../da/d98/sketch_8ino.html#afe461d27b9c48d5921c00d521181f12f',1,'sketch.ino']]]
+  ['setup_0',['setup',['../d6/d3b/zad_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'zad.ino']]]
 ];
