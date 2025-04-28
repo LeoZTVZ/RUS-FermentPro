9b0ba4556bb0ba089facfc465d7bebf6eaed2f5b
AntonioMiser0 <amiser@tvz.hr>
2025-04-28 08:03:46 +0200

    Arduino kod

--- projekt/projekt.ino
+++ projekt/projekt.ino
@@ -1,4 +1,13 @@
-const int ledPin = 2;    // kontrola opto LED emitter
+#include "DHT.h"
+
+#define DHTPIN 2       // Data pin
+#define DHTPOW 11       // Power pin (controls VCC)
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long previousMillis = 0;
+const long interval2 = 5 * 60 * 1000UL;  // 5 minutes
 const int sensorPin = 7; // pin kolektora
 int count = 0;           // brojač broja mjehurića
 unsigned long lastResetTime = 0;
@@ -8,15 +17,35 @@ bool lastState = HIGH;
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
   pinMode(sensorPin, INPUT);
   Serial.begin(9600);
   lastResetTime = millis();
-  digitalWrite(ledPin, HIGH);
+  pinMode(DHTPOW, OUTPUT);
 }
 
 void loop()
 {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval2) {
+    previousMillis = currentMillis;
+
+    // Power up the sensor
+    digitalWrite(DHTPOW, HIGH);
+    delay(2000);
+
+    dht.begin(); 
+
+    float temperature = dht.readTemperature();
+
+    if (isnan(temperature)) {
+      Serial.println("Pogreška čitanja sa senzora!");
+    } else {
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+      Serial.println(" °C");
+    }
+    digitalWrite(DHTPOW, LOW);
+  }
   // čitanje stanja kolektora
   bool currentState = digitalRead(sensorPin);
 
