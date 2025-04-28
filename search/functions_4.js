8c2408fe811fe97b2afc5c38018162b7490a4dae
LeoZTVZ <LeoZTVZ@users.noreply.github.com>
2025-04-28 10:42:13 +0000

    deploy: 10ac32e95dafab02b8a68a6dea9b0c897b58b715

--- search/functions_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['timer1setup_0',['Timer1Setup',['../da/d98/sketch_8ino.html#a6da768d9996fe5c028404077274e5b4b',1,'sketch.ino']]],
-  ['toggleled_1',['toggleLED',['../da/d98/sketch_8ino.html#a4bbc5d148cb44b6581eb6cdf3ac86534',1,'sketch.ino']]]
-];
