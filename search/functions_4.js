88b4c78aee8b39c51642cb83fe2aa418b99c3750
amiser11 <amiser11@users.noreply.github.com>
2025-04-28 06:04:23 +0000

    deploy: 9b0ba4556bb0ba089facfc465d7bebf6eaed2f5b

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['timer1setup_0',['Timer1Setup',['../da/d98/sketch_8ino.html#a6da768d9996fe5c028404077274e5b4b',1,'sketch.ino']]],
-  ['toggleled_1',['toggleLED',['../da/d98/sketch_8ino.html#a4bbc5d148cb44b6581eb6cdf3ac86534',1,'sketch.ino']]]
+  ['setup_0',['setup',['../da/d98/sketch_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'sketch.ino']]],
+  ['setupwatchdog_1',['setupWatchdog',['../da/d98/sketch_8ino.html#a476ccd62c199f330f95bce19c498e3e9',1,'sketch.ino']]]
 ];
