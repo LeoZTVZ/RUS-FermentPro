2bce96f4bb30fc25207142038be530accd2b62fc
leoz2311 <103137352+leoz2311@users.noreply.github.com>
2025-04-28 14:09:35 +0200

    Dodani komentari za doxygen dokumentaciju, dodan lightsleep, dodan LCD

--- projekt/arduino_secrets.h
+++ projekt/arduino_secrets.h
@@ -1,4 +1,4 @@
 #define SECRET_SSID "Leov laptop"
-#define SECRET_PASS "x"
+#define SECRET_PASS "0989691301"
 #define SECRET_HOST "smartferment-default-rtdb.europe-west1.firebasedatabase.app"
-#define SECRET_AUTH "x"
+#define SECRET_AUTH "4oR9yfV1WPHreFi7pVG2yfLJpaR8NbkfWv1W5aUL"
--- projekt/projekt.ino
+++ projekt/projekt.ino
@@ -1,173 +1,178 @@
+/**
+ * @file projekt.ino
+ * @brief Glavni Arduino kod za rad s Firebaseom, očitavanje podataka sa senzora i prikazivanje na LCD ekranu.
+ *
+ * Ovaj projekt koristi DHT22 senzor za očitavanje temperature, foto senzor za detekciju mjehurića i LCD ekran za prikaz podataka.
+ * Također, podaci se šalju na Firebase i koristi se API za dohvat trenutnog vremena.
+ */
 
-#include "DHT.h"
-#include <WiFiNINA.h>
-#include "Firebase_Arduino_WiFiNINA.h"
-#include <WiFiSSLClient.h>
-#include <ArduinoHttpClient.h>
-#include "arduino_secrets.h"
-
-// WiFi credentials
-#define WIFI_SSID SECRET_SSID
-#define WIFI_PASSWORD SECRET_PASS
-
-// Firebase credentials
-#define FIREBASE_HOST SECRET_HOST
-#define FIREBASE_AUTH SECRET_AUTH
-#define DHTPIN 2  // Data pin
-#define DHTPOW 11 // Power pin (controls VCC)
-#define DHTTYPE DHT22
-
-DHT dht(DHTPIN, DHTTYPE);
-FirebaseData firebaseData;
-
-unsigned long previousMillis = 0;
-const long interval2 = 10000UL; // 5 minutes
-const int sensorPin = 7;        // pin kolektora
-int count = 0;                  // brojač broja mjehurića
-unsigned long lastResetTime = 0;
-const unsigned long interval = 10000; // vremensko razdoblje za ispis broja mjehurića
-int temp = 0;
-bool lastState = HIGH;
-
-char server[] = "www.timeapi.io";
-int port = 443; // HTTPS
-
-String path1 = "/api/Time/current/zone?timeZone=UTC";
-
-WiFiSSLClient wifi; // <-- important to use SSL client
-HttpClient client = HttpClient(wifi, server, port);
-
+/**
+ * @brief Funkcija koja se izvodi na početku i inicijalizira WiFi, Firebase, DHT senzor i LCD ekran.
+ *
+ * Ova funkcija se izvodi samo jednom prilikom pokretanja i postavlja sve potrebne inicijalizacije, uključujući serijsku komunikaciju,
+ * WiFi, Firebase, DHT senzor i LCD ekran.
+ */
 void setup()
 {
   Serial.begin(115200);
-  Serial.print("Connecting to WiFi");
+  while (!Serial)
+    ;
+
+  pinMode(sensorPin, INPUT);
+
+  connectWiFiAndFirebase();
+  dht.begin();
+
+  lcd.begin(16, 2);
+  lcd.setBacklight(1); // Uključi pozadinsko osvjetljenje
+  lcd.print("Inicijalizacija...");
+}
+
+/**
+ * @brief Glavna petlja koja kontinuirano čita podatke sa senzora i prikazuje ih na LCD ekranu.
+ *
+ * Ova funkcija čita podatke temperature sa DHT22 senzora i broji mjehuriće putem foto senzora.
+ * Zatim prikazuje te podatke na LCD ekranu, pohranjuje ih na Firebase i čeka jednu minutu prije nego što ponovi postupak.
+ */
+void loop()
+{
+  // Očitavanje podataka
+  float temperature = dht.readTemperature();
+  if (isnan(temperature))
+  {
+    Serial.println("Greška pri očitavanju temperature!");
+    temperature = -99.0;
+  }
+
+  int count = readBubblesFor10Seconds();
+
+  Serial.print("Broj mjehurića: ");
+  Serial.println(count);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Bubbles: ");
+  lcd.print(count);
+  upis_u_bazu(count, temperature);
+
+  // Isključivanje WiFi-a radi uštede energije (opcionalno)
+  WiFi.disconnect();
+  WiFi.end();
+
+  // Mirovanje 1 minut
+  Serial.println("Čekanje 1 min...");
+  unsigned long sleepStart = millis();
+  while (millis() - sleepStart < 60000UL)
+  {
+    delay(100); // Mali odmor kako bi se spriječilo preopterećenje CPU-a
+  }
+
+  // Ponovno spajanje na WiFi nakon mirovanja
+  connectWiFiAndFirebase();
+}
+
+/**
+ * @brief Funkcija za povezivanje na WiFi mrežu i Firebase bazu podataka.
+ *
+ * Spaja se na WiFi mrežu koristeći SSID i lozinku, a zatim inicijalizira vezu s Firebaseom.
+ */
+void connectWiFiAndFirebase()
+{
+  Serial.print("Spajanje na WiFi");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(100);
     Serial.print(".");
   }
-  Serial.println("WiFi connected.");
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  pinMode(sensorPin, INPUT);
-  // Only now: start UDP, Firebase, HTTP client
-  // Connect to Firebase
+  Serial.println(" Spojeno!");
+
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD);
   Firebase.reconnectWiFi(true);
-
-  lastResetTime = millis();
 }
 
-void loop()
+/**
+ * @brief Funkcija koja očitava broj mjehurića sa foto senzora tijekom 10 sekundi.
+ *
+ * Funkcija mjeri promjene stanja foto senzora i broji koliko puta se stanje promijeni (detektira prolazak mjehurića).
+ *
+ * @return Broj detektiranih mjehurića u razdoblju od 10 sekundi.
+ */
+int readBubblesFor10Seconds()
 {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval2)
+  int bubbleCount = 0;
+  bool currentState;
+  lastState = HIGH;
+
+  unsigned long startTime = millis();
+  while (millis() - startTime < 10000)
   {
-    previousMillis = currentMillis;
+    currentState = digitalRead(sensorPin);
 
-    // Power up the sensor
-    delay(2000);
-
-    dht.begin();
-
-    float temperature = dht.readTemperature();
-
-    if (isnan(temperature))
+    if (lastState == HIGH && currentState == LOW)
     {
-      Serial.println("Pogreška čitanja sa senzora!");
+      bubbleCount++;
+      delay(50); // Debouncing
     }
-    else
-    {
-      Serial.print("Temperature: ");
-      Serial.print(temperature);
-      Serial.println(" °C");
-      upis_u_bazu(count, temperature);
-    }
+    lastState = currentState;
   }
-  // čitanje stanja kolektora
-  bool currentState = digitalRead(sensorPin);
-
-  // ako je stanje promijenjeno znači da je učitan prekid svijetlosti
-  if (lastState == HIGH && currentState == LOW)
-  {
-    count++;
-    delay(50); // debounce
-  }
-
-  lastState = currentState;
-
-  // Provjera proteklog vremena
-  if (millis() - lastResetTime >= interval)
-  {
-    print();
-  }
-}
-// Funkcija za ispis izbrojanih prekida
-void print()
-{
-  Serial.print("Broj mjehurica u zadnjih 10s: ");
-  Serial.println(count);
-
-  count = 0;
-  lastResetTime = millis();
+  return bubbleCount;
 }
 
+/**
+ * @brief Funkcija koja unosi podatke o mjehurićima i temperaturi u Firebase bazu podataka.
+ *
+ * Ova funkcija kreira jedinstveni ID za svaku pohranu podataka, dohvaća trenutno vrijeme putem HTTP API-ja,
+ * te unosi podatke u Firebase u odgovarajuće polje.
+ *
+ * @param mjeh Broj mjehurića detektiranih tijekom mjerenja.
+ * @param temp Temperatura očitana sa DHT senzora.
+ */
 void upis_u_bazu(int mjeh, float temp)
 {
   long id = random(1000, 9999) + millis();
   Serial.println(id);
-  String recordId = String(id); // You could generate random IDs or timestamps
+  String recordId = String(id);
 
-  // Build the path
   String path = "/fermentRecord/" + recordId;
   String datetime = getTimeFromHTTP();
   String formatted = convertToFormattedTime(datetime);
+
   if (Firebase.setString(firebaseData, path + "/dateTime", formatted))
-  {
-    Serial.println("dateTime uploaded successfully!");
-  }
+    Serial.println("dateTime uspješno prenesen!");
   else
-  {
-    Serial.print("Failed to upload dateTime: ");
-    Serial.println(firebaseData.errorReason());
-  }
+    Serial.print("Neuspješno slanje dateTime: "), Serial.println(firebaseData.errorReason());
 
   if (Firebase.setString(firebaseData, path + "/deviceId", "asw11"))
-  {
-    Serial.println("deviceId uploaded successfully!");
-  }
+    Serial.println("deviceId uspješno prenesen!");
   else
-  {
-    Serial.print("Failed to upload deviceId: ");
-    Serial.println(firebaseData.errorReason());
-  }
+    Serial.print("Neuspješno slanje deviceId: "), Serial.println(firebaseData.errorReason());
 
   if (Firebase.setInt(firebaseData, path + "/photoSensor", mjeh))
-  {
-    Serial.println("photoSensor uploaded successfully!");
-  }
+    Serial.println("photoSensor uspješno prenesen!");
   else
-  {
-    Serial.print("Failed to upload photoSensor: ");
-    Serial.println(firebaseData.errorReason());
-  }
+    Serial.print("Neuspješno slanje photoSensor: "), Serial.println(firebaseData.errorReason());
 
   if (Firebase.setFloat(firebaseData, path + "/temperature", temp))
-  {
-    Serial.println("temperature uploaded successfully!");
-  }
+    Serial.println("temperature uspješno prenesen!");
   else
-  {
-    Serial.print("Failed to upload temperature: ");
-    Serial.println(firebaseData.errorReason());
-  }
+    Serial.print("Neuspješno slanje temperature: "), Serial.println(firebaseData.errorReason());
 }
 
+/**
+ * @brief Funkcija za dohvat trenutnog vremena sa HTTP API-ja.
+ *
+ * Povezuje se na API "timeapi.io" i dohvaća UTC vremensku oznaku u formatu ISO 8601.
+ *
+ * @return Trenutno vrijeme u formatu "yyyy-MM-ddTHH:mm:ss".
+ */
 String getTimeFromHTTP()
 {
-  Serial.println("Requesting time from timeapi.io...");
+  Serial.println("Dohvaćanje vremena s timeapi.io...");
   String datetime = "0";
   client.get(path1);
 
@@ -176,10 +181,9 @@ String getTimeFromHTTP()
 
   if (statusCode == 200)
   {
-    Serial.println("Got response:");
+    Serial.println("Dohvaćeni podaci:");
     Serial.println(response);
 
-    // Simple parsing: find "dateTime" field
     int index = response.indexOf("\"dateTime\":\"");
     if (index >= 0)
     {
@@ -187,18 +191,27 @@ String getTimeFromHTTP()
       int end = response.indexOf("\"", start);
       datetime = response.substring(start, end);
 
-      Serial.print("Current UTC DateTime: ");
+      Serial.print("Trenutno UTC vrijeme: ");
       String formatted = convertToFormattedTime(datetime);
       Serial.println(formatted);
     }
   }
   else
   {
-    Serial.print("Failed to get time, status code: ");
+    Serial.print("Neuspješan zahtjev, status kod: ");
     Serial.println(statusCode);
   }
   return datetime;
 }
+
+/**
+ * @brief Funkcija koja konvertira UTC vrijeme u formatirani string.
+ *
+ * Ova funkcija uzima UTC vrijeme u formatu "yyyy-MM-ddTHH:mm:ss" i konvertira ga u format "HH:mm:ss dd.MM.yyyy".
+ *
+ * @param datetime UTC vrijeme u formatu "yyyy-MM-ddTHH:mm:ss".
+ * @return Formatirani string vremena.
+ */
 String convertToFormattedTime(String datetime)
 {
   String year = datetime.substring(0, 4);
